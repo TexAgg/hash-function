ae18ac51ac8fce515ec3b3807966fed1068b81ac
Matt G <matt.gaikema@tamu.edu>
2016-03-28 21:34:53 -0500

    Initial ShareLaTeX Import

---
+++ main.R
@@ -0,0 +1,24 @@
+#Clear previous variables
+rm(list=ls())
+
+h = function(k){
+    return((3*k+5) %% 11)
+}
+
+lin_prob = function(vec, hash, elem){
+    if (hash(elem) %in% vec){
+        lin_prob(vec, hash, elem+1)
+    }
+    else{
+        print(elem)
+        vec = c(vec, hash(elem))
+    }
+}
+
+k = c(12, 44, 13, 88, 23, 94, 11, 39, 20, 16, 5)
+h_k = c()
+
+for (i in 1:length(k)){
+    lin_prob(h_k, h, k[i])
+    #print(k[i])
+}
